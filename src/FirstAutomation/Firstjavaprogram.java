319506312b336612c8ad0c7164c67b82f878a425
AjayPenchala <ajaypenchala9@gmail.com>
2024-07-21 00:44:47 -0400

    Update Firstjavaprogram.java

--- src/FirstAutomation/Firstjavaprogram.java
+++ src/FirstAutomation/Firstjavaprogram.java
@@ -7,9 +7,11 @@ public class Firstjavaprogram
 	{
 		int a = 123455;
 		byte b = 20;
+		int sum = a+b;
 		System.out.println("hello world");
 		System.out.println("the sum is " + (a+b));
 		System.out.println(a+b);
+		System.out.println(sum);
 		/*System.out.println("test");
 		System.out.println("test");
 		System.out.println("test");*/
